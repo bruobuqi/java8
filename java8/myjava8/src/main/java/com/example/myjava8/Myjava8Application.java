9acbe6d5a43a14cf5e4cef9e38ac2997a1d1b823
bruobuqi <1193921715@qq.com>
2019-03-27 21:22:48 +0800

    新建项目myjava8

---
+++ java8/myjava8/src/main/java/com/example/myjava8/Myjava8Application.java
@@ -0,0 +1,13 @@
+package com.example.myjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myjava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myjava8Application.class, args);
+    }
+
+}
